1e06e85e1a6b673068ef08522bb5591311476478
Misge <tesfaymisgana@gmail.com>
2022-10-21 23:22:41 +0300

    todo serverless application

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,36 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
+import { getAllTodo } from '../../businessLogic/todos'
+
+// TODO: Get all TODO items for a current user
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // Write your code here
+
+    const authorization = event.headers.Authorization;
+    const split = authorization.split(' ');
+    const jwtToken = split[1];
+
+    const toDos = await getAllTodo(jwtToken);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+            "items": toDos,
+        }),
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
